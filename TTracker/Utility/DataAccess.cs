8c81f0ddb1025d7fd3b46f90eb160f2518b7263c
TheItCrOw <keboen@web.de>
2019-04-25 15:50:35 +0200

    Rewrote the xmlWriter to make it more generic

--- TTracker/Utility/DataAccess.cs
+++ TTracker/Utility/DataAccess.cs
@@ -22,7 +22,7 @@ namespace TTracker.Utility
         private string _saveDataPath = System.AppDomain.CurrentDomain.BaseDirectory + "..\\..\\Data\\";
 
         //Keeps track of all the registeredUsers
-        private User SaveableUser;
+        private List<string> SaveableDataList = new List<string>();
 
         private DataAccess()
         {
@@ -32,31 +32,43 @@ namespace TTracker.Utility
         public void RegisterUser(Object newUser)
         {
             var _newUser = (User)newUser;
-            SaveableUser = _newUser;
-            SaveUser();
-            //Clear the password out of the memory
+            SaveableDataList.Clear();
+            SaveableDataList.Add("Id/" + _newUser.Id);
+            SaveableDataList.Add("Name/" + _newUser.Name);
+            SaveableDataList.Add("Password/" + _newUser.Password);
+            SaveableDataList.Add("Created/" + _newUser.Created.ToString());
+            SaveToXml("Users", _newUser.Id, SaveableDataList);
+            //Clear the password out of memory
             _newUser.Password.Remove(0, PasswordBoxAssistant.PasswordContent.Length);
             PasswordBoxAssistant.PasswordContent.Remove(0, PasswordBoxAssistant.PasswordContent.Length);
         }
 
-        //This writes an xml file of the user
-        public void SaveUser()
+        /// <summary>
+        /// Writes the data that is given into the list into a xml file. 
+        /// Split like this: Name/paul
+        /// </summary>
+        /// <param name="directoryName"></param>
+        /// <param name="data"></param>
+        public void SaveToXml(string directoryName, Guid ID, List<string> data)
         {
-            Directory.CreateDirectory(_saveDataPath + "Users");
-
-            string xmlPath = _saveDataPath + "Users\\";
-            string xmlName = SaveableUser.Id.ToString() + ".xml";
+            Directory.CreateDirectory(_saveDataPath + directoryName);
+            string xmlPath = _saveDataPath + directoryName + "\\";
+            string xmlName = ID.ToString() + ".xml";
             string fullLocationPath = xmlPath + xmlName;
 
             using (XmlWriter writer = XmlWriter.Create(fullLocationPath))
             {
                 writer.WriteStartDocument();
-                writer.WriteStartElement("User");
+                writer.WriteStartElement("Data");
 
-                writer.WriteElementString("ID", SaveableUser.Id.ToString());
-                writer.WriteElementString("Name", SaveableUser.Name);
-                writer.WriteElementString("Password", SaveableUser.Password);
-                writer.WriteElementString("Created", SaveableUser.Created.ToString());
+                foreach (var s in data)
+                {
+                    string[] splitedString = s.Split(new char[] { '/' });
+                    string dataName = splitedString[0];
+                    string dataValue = splitedString[1];
+
+                    writer.WriteElementString(dataName, dataValue);
+                }
 
                 writer.WriteEndElement();
                 writer.WriteEndDocument();
