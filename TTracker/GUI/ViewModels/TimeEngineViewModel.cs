ba82d2e41655f199802676c4c42235c24a10f0bf
TheItCrOw <keboen@web.de>
2019-06-04 20:15:03 +0200

    Continued on the time Engine

--- TTracker/GUI/ViewModels/TimeEngineViewModel.cs
+++ TTracker/GUI/ViewModels/TimeEngineViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TTracker.BaseDataModules;
+using TTracker.Utility;
 
 namespace TTracker.GUI.ViewModels
 {
-    class TimeEngineViewModel
+    class TimeEngineViewModel : ViewModelManagementBase
     {
+        private string _introText;
+
+        public string IntroText
+        {
+            get { return _introText; }
+            set
+            {
+                SetProperty(ref _introText, value);
+            }
+        }
+
+
+        public TimeEngineViewModel()
+        {
+            GetCurrentUser();
+        }
+
+        void GetCurrentUser()
+        {
+            IntroText = "Hello " + DataAccess.CurrentLoggedUser.Name + "! What are you gonna do today, on the " + DateTime.Now.ToShortDateString() + "?";
+        }
+
     }
 }
